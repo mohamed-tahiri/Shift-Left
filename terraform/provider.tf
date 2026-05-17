a4e4b757d5c18ab4d9a6db89902affeae196a393
mohamed.tahiri <mhdtahiri01@gmail.com>
2026-05-17 17:48:48 +0200

    security: update gitignore to exclude tfstate files and untrack local state

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,12 +1,21 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+    source  = "hashicorp/aws"
+    version = "~> 5.0"
     }
+  }
+
+  backend "s3" {
+    bucket         = "devsecops-terraform-state-768669379117"
+    key            = "devsecops-project/terraform.tfstate"
+    region         = "eu-west-3"
+    dynamodb_table = "devsecops-terraform-locks"
+    encrypt        = true
   }
 }
 
 provider "aws" {
-  region = "eu-west-3" 
+  region = "eu-west-3"
 }
+
